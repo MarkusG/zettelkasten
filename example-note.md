62399da36e7069479ebf5059f10e4d4d3d481664
Mark Gross <mark.gross2001@gmail.com>
2020-03-06 16:43:18 -0800

    Initial commit

---
+++ example-note.md
@@ -0,0 +1,6 @@
+title: The C Programming Language
+tags: programming c cs
+
+# The C Programming Language
+
+It's the best language in the world and super old. It was created to increase portability compared to assembly.
---
+++ insert-note.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+# get full path to file
+path=$(realpath $1)
+
+# get title      vvvvvvvvvvvv only first occurrence
+title=$(sed -En '0,/^title: / s/^title: (.*)$/\1/p' < $path)
+
+# get raw list of tags
+tags_unsorted=($(sed -En '0,/^tags: / s/^tags: (.*)$/\1/p' < $path))
+
+# check if tags exist
+if [[ -n $tags_unsorted ]]; then
+	# sort 'em
+	tags=($(for i in "${tags_unsorted[@]}"; do echo $i; done | sort))
+	# format them as {"tag1", "tag2", ..., "tagn"}
+	tag_string="{"
+	for i in ${!tags[@]}
+	do
+		tag=${tags[$i]}
+		tag_string="${tag_string}\"${tag}\""
+		[[ $i -lt $((${#tags[@]} - 1)) ]] && tag_string="${tag_string}, "
+	done
+	tag_string="${tag_string}}"
+fi
+
+# holy SQL injection, batman!
+# if the user wants to SQL inject their own zettelkasten,
+# quite honestly, that's their prerogative
+# form SQL command string
+if [[ -n $tag_string ]]; then
+	psql_query="INSERT INTO note (title, tags, file)
+	            VALUES ('${title}', '${tag_string}', '${path}');"
+else
+	psql_query="INSERT INTO note (title, file)
+	            VALUES ('${title}', '${path}');"
+fi
+
+echo $psql_query | psql -d zettelkasten
